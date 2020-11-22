4be69982239aacc10a9230d2d7afb5dfe7657ff8
Joknaa <54961980+Joknaa@users.noreply.github.com>
2020-11-22 22:30:42 +0100

    TP_Sol_2 v0.19a
    I HATE THE FORMATTING IN ORACLE SQL DEVELOPER !

--- TP_Sol_2.sql
+++ TP_Sol_2.sql
@@ -1,16 +1,15 @@
 SET SERVEROUTPUT ON;
 --Q1
-
       DECLARE
-            v_ID                           EMPLOYEES.EMPLOYEE_ID%TYPE;
-            v_FirstName             EMPLOYEES.FIRST_NAME%TYPE;
-            v_LastName              EMPLOYEES.LAST_NAME%TYPE;
-            v_Email                      EMPLOYEES.EMAIL%TYPE;
-            v_Phone                    EMPLOYEES.PHONE%TYPE;
-            v_HireDate               EMPLOYEES.HIRE_DATE%TYPE;
-            v_ManagerID           EMPLOYEES.MANAGER_ID%TYPE;
-            v_JobTitle                 EMPLOYEES.JOB_TITLE%TYPE;
-            v_ManagerName     EMPLOYEES.LAST_NAME%TYPE;
+            v_ID EMPLOYEES.EMPLOYEE_ID%TYPE;
+            v_FirstName EMPLOYEES.FIRST_NAME%TYPE;
+            v_LastName EMPLOYEES.LAST_NAME%TYPE;
+            v_Email EMPLOYEES.EMAIL%TYPE;
+            v_Phone EMPLOYEES.PHONE%TYPE;
+            v_HireDate EMPLOYEES.HIRE_DATE%TYPE;
+            v_ManagerID EMPLOYEES.MANAGER_ID%TYPE;
+            v_JobTitle EMPLOYEES.JOB_TITLE%TYPE;
+            v_ManagerName EMPLOYEES.LAST_NAME%TYPE;
             CURSOR c_EmployeeSummary IS 
                   SELECT * FROM EMPLOYEES;
       BEGIN
