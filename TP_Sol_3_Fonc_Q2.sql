5cf710373e49afe1c05e3e97a3dee6929f112d81
Joknaa <54961980+Joknaa@users.noreply.github.com>
2020-11-27 02:57:58 +0100

    TP_Sol_3 v2.6

---
+++ TP_Sol_3_Fonc_Q2.sql
@@ -0,0 +1,14 @@
+DECLARE
+    v_TotalPendingOrders   NUMBER;
+
+    FUNCTION f_CountPendingOrders RETURN NUMBER IS
+        v_TotalPendingOrders NUMBER;
+    BEGIN
+        SELECT count(ORDER_ID) INTO v_TotalPendingOrders FROM ORDERS WHERE STATUS = 'Pending';
+        RETURN v_TotalPendingOrders;
+    END f_CountPendingOrders;
+
+BEGIN
+    v_TotalPendingOrders := f_CountPendingOrders();
+    DBMS_OUTPUT.PUT_LINE('The Number of orders with status "Pending" is: ' || v_TotalPendingOrders);
+END;
